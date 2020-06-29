8f3a5b13906587e7e665d7488e664b1b17b0a350
Kingston Chan <c.kingston@univapay.com>
2020-06-29 21:14:26 +0900

    WIP

--- src/Univapay/Enums/CardCategory.php
+++ src/Univapay/Enums/CardCategory.php
@@ -6,6 +6,14 @@ final class CardCategory extends TypedEnum
 {
     // phpcs:disable
     public static function CLASSIC() { return self::create(); }
+    public static function GOLD() { return self::create(); }
+    public static function TITANIUM() { return self::create(); }
+    public static function PLATINIUM() { return self::create(); }
+    public static function ATM() { return self::create(); }
+    public static function ELECTRON() { return self::create(); }
+    public static function MAESTRO() { return self::create(); }
+    public static function WORLD() { return self::create(); }
     public static function CORPORATE() { return self::create(); }
+    public static function BUSINESS() { return self::create(); }
     public static function PREPAID() { return self::create(); }
 }
--- src/Univapay/Enums/Gateway.php
+++ src/Univapay/Enums/Gateway.php
@@ -15,16 +15,18 @@ final class Gateway extends TypedEnum
     public static function FIRST_DATA() { return self::create(); }
     public static function QQ() { return self::create(); }
     public static function ALIPAY() { return self::create(); }
-    public static function WE_CHAT() { return self::create(); }
+    public static function ALIPAY_MERCHANT_QR() { return self::create(); }
     public static function ALIPAY_CONNECT() { return self::create(); }
+    public static function WE_CHAT() { return self::create(); }
+    public static function BARTONG() { return self::create(); }
+    public static function JKOPAY() { return self::create(); }
+    public static function VIA() { return self::create(); }
     public static function D_BARAI() { return self::create(); }
     public static function ORIGAMI() { return self::create(); }
     public static function MERPAY() { return self::create(); }
     public static function RAKUTEN_PAY() { return self::create(); }
-    public static function BARTONG() { return self::create(); }
-    public static function JKOPAY() { return self::create(); }
-    public static function VIA() { return self::create(); }
-    public static function ALIPAY_MERCHANT_QR() { return self::create(); }
+    public static function PAY_PAY() { return self::create(); }
+    public static function LINE_PAY() { return self::create(); }
     public static function DENSAN() { return self::create(); }
     public static function PAIDY() { return self::create(); }
     public static function IPS() { return self::create(); }
