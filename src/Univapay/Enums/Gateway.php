b7c4d799524e54b5883815b8d7ced87bf040e981
Kingston Chan <c.kingston@univapay.com>
2020-12-10 14:17:34 +0900

    Type does not exist on Charge model (#10)
    * Type does not exist on Charge model
    
    * Add new gateways and remove test gateway

--- src/Univapay/Enums/Gateway.php
+++ src/Univapay/Enums/Gateway.php
@@ -16,27 +16,30 @@ final class Gateway extends TypedEnum
     public static function NCCC() { return self::create(); }
     public static function MEIKO_PAY() { return self::create(); }
     public static function FIRST_DATA() { return self::create(); }
+    public static function SMART_CHECKOUT() { return self::create(); }
 
     public static function QQ() { return self::create(); }
     public static function ALIPAY() { return self::create(); }
-    public static function ALIPAY_MERCHANT_QR() { return self::create(); }
     public static function ALIPAY_CONNECT() { return self::create(); }
     public static function WE_CHAT() { return self::create(); }
     public static function BARTONG() { return self::create(); }
     public static function JKOPAY() { return self::create(); }
     public static function VIA() { return self::create(); }
     public static function D_BARAI() { return self::create(); }
+    public static function AU_PAY() { return self::create(); }
     public static function ORIGAMI() { return self::create(); }
     public static function MERPAY() { return self::create(); }
     public static function RAKUTEN_PAY() { return self::create(); }
     public static function PAY_PAY() { return self::create(); }
     public static function LINE_PAY() { return self::create(); }
+    
+    public static function ALIPAY_MERCHANT_QR() { return self::create(); }
+    public static function PAY_PAY_MERCHANT() { return self::create(); }
 
     public static function DENSAN() { return self::create(); }
 
     public static function PAIDY() { return self::create(); }
 
     public static function ALIPAY_ONLINE() { return self::create(); }
-    
-    public static function TEST() { return self::create(); }
+    public static function PAY_PAY_ONLINE() { return self::create(); }
 }
--- src/Univapay/Enums/QrBrandMerchant.php
+++ src/Univapay/Enums/QrBrandMerchant.php
@@ -6,4 +6,5 @@ final class QrBrandMerchant extends TypedEnum
 {
     // phpcs:disable
     public static function ALIPAY_MERCHANT_QR() { return self::create(); }
+    public static function PAY_PAY_MERCHANT() { return self::create(); }
 }
