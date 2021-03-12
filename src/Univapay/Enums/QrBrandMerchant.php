2df57cd112b6a83d77fd352e7ab27ccee60d1ba7
Kingston Chan <c.kingston@univapay.com>
2021-03-12 13:43:51 +0900

    Update brands and payment methods

--- src/Univapay/Enums/QrBrandMerchant.php
+++ src/Univapay/Enums/QrBrandMerchant.php
@@ -6,5 +6,6 @@ final class QrBrandMerchant extends TypedEnum
 {
     // phpcs:disable
     public static function ALIPAY_MERCHANT_QR() { return self::create(); }
+    public static function ALIPAY_CONNECT_MPM() { return self::create(); }
     public static function PAY_PAY_MERCHANT() { return self::create(); }
 }
