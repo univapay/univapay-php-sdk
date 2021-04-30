492b024eb7860364c6a4e3bec5e49e462714be26
Kingston Chan <c.kingston@univapay.com>
2021-04-30 16:46:48 +0900

    Update rmccue/requests & add new brands and gateways (#14)

--- src/Univapay/Enums/QrBrandMerchant.php
+++ src/Univapay/Enums/QrBrandMerchant.php
@@ -5,7 +5,11 @@ namespace Univapay\Enums;
 final class QrBrandMerchant extends TypedEnum
 {
     // phpcs:disable
-    public static function ALIPAY_MERCHANT_QR() { return self::create(); }
     public static function ALIPAY_CONNECT_MPM() { return self::create(); }
+    public static function ALIPAY_CHINA() { return self::create(); }
+    public static function ALIPAY_HK() { return self::create(); }
+    public static function ALIPAY_SINGAPORE() { return self::create(); }
+    public static function KAKAOPAY() { return self::create(); }
+    public static function ALIPAY_MERCHANT_QR() { return self::create(); }
     public static function PAY_PAY_MERCHANT() { return self::create(); }
 }
