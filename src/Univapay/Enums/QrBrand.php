951caf493f3f8ba4af073678b26ad3b4328c78ab
Kingston Chan <c.kingston@univapay.com>
2021-04-30 16:22:10 +0900

    Update rmccue/requests & add new brands and gateways

--- src/Univapay/Enums/QrBrand.php
+++ src/Univapay/Enums/QrBrand.php
@@ -27,4 +27,15 @@ final class QrBrand extends TypedEnum
     public static function MERPAY() { return self::create(); }
     public static function RAKUTEN_PAY() { return self::create(); }
     public static function LINE_PAY() { return self::create(); }
+    public static function GINKO_PAY() { return self::create(); }
+    public static function YUCHO_PAY() { return self::create(); }
+    public static function HAMA_PAY() { return self::create(); }
+    public static function OKI_PAY() { return self::create(); }
+    public static function YOKA_PAY_FUKUOKA() { return self::create(); }
+    public static function YOKA_PAY_KUMAMOTO() { return self::create(); }
+    public static function YOKA_PAY_SHINWA() { return self::create(); }
+    public static function HOKUHOKU_PAY_HOKKAIDO() { return self::create(); }
+    public static function HOKUHOKU_PAY_HOKURIKU() { return self::create(); }
+    public static function COI_PAY_HIROSHIMA() { return self::create(); }
+    public static function UNKNOWN() { return self::create(); }
 }
