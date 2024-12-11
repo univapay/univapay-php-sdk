85f87ab511d6ba62073dbbb5a84c671b00a098d2
Roy Wijaya <r.wijaya@univapay.com>
2024-12-11 13:50:50 +0900

    update 3ds modes

--- src/Univapay/Enums/ThreeDSMode.php
+++ src/Univapay/Enums/ThreeDSMode.php
@@ -5,8 +5,10 @@ namespace Univapay\Enums;
 final class ThreeDSMode extends TypedEnum
 {
     // phpcs:disable
-    public static function NORMAL() { return self::create(); }
-    public static function REQUIRE() { return self::create(); }
     public static function FORCE() { return self::create(); }
+    public static function IF_AVAILABLE() { return self::create(); }
+    public static function NORMAL() { return self::create(); }
+    public static function PROVIDED() { return self::create(); }
+    public static function REQUIRE() { return self::create(); }
     public static function SKIP() { return self::create(); }
 }
