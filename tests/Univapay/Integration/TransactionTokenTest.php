52c74bfd115284b92be95e81bf40d5562d7812b8
Roy Wijaya <r.wijaya@univapay.com>
2024-12-28 11:24:48 +0900

    Enabled and Disabled 3DS on recurring tokens (#74)
    * transaction token should be able to enable / disable 3DS
    
    * fix should return as same object type

--- tests/Univapay/Integration/TransactionTokenTest.php
+++ tests/Univapay/Integration/TransactionTokenTest.php
@@ -108,6 +108,34 @@ class TransactionTokenTest extends TestCase
         $this->assertEquals(PaymentType::CARD(), $threeDSIssuerToken->paymentType);
     }
 
+    public function testEnableDisableThreeDS()
+    {
+        $mockRedirectUrl = "https://test.int/endpoint?foo=bar";
+        $transactionToken = $this->createValidToken(
+            PaymentType::CARD(),
+            TokenType::RECURRING()
+        );
+
+        $transactionToken = $transactionToken->enableThreeDS(true, $mockRedirectUrl)->awaitResult(5);
+        $this->assertTrue($transactionToken->data->threeDS->enabled);
+        $this->assertEquals($mockRedirectUrl, $transactionToken->data->threeDS->redirectEndpoint);
+        $this->assertNotNull($transactionToken->data->threeDS->redirectId);
+        $this->assertEquals(ThreeDSStatus::AWAITING(), $transactionToken->data->threeDS->status);
+        $this->assertNull($transactionToken->data->threeDS->error);
+
+        // Confirm 3DS Issuer Token
+        $threeDSIssuerToken = $transactionToken->threeDSIssuerToken();
+        $this->assertEquals(CallMethod::HTTP_POST(), $threeDSIssuerToken->callMethod);
+        $this->assertNotNull($threeDSIssuerToken->contentType);
+        $this->assertIsString($threeDSIssuerToken->issuerToken);
+        $this->assertNotNull($threeDSIssuerToken->payload);
+        $this->assertEquals(PaymentType::CARD(), $threeDSIssuerToken->paymentType);
+
+        $transactionToken = $transactionToken->enableThreeDS(false);
+        $this->assertFalse($transactionToken->data->threeDS->enabled);
+        $this->assertNull($transactionToken->data->threeDS->redirectEndpoint);
+    }
+
     public function testCreateTokenWithCvvAuth()
     {
         $cvvAuth = new CvvAuthorize(true);
