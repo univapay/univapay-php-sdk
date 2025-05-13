e05256227b50b15e1a716cef53d7e0256b7be15f
Roy Wijaya <r.wijaya@univapay.com>
2025-05-13 18:14:21 +0900

    add live test token mode

--- src/Univapay/Enums/AppTokenMode.php
+++ src/Univapay/Enums/AppTokenMode.php
@@ -6,5 +6,6 @@ final class AppTokenMode extends TypedEnum
 {
     // phpcs:disable
     public static function TEST() { return self::create(); }
+    public static function LIVE_TEST() { return self::create(); }
     public static function LIVE() { return self::create(); }
 }
